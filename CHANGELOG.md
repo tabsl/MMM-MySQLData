28c5bc3fa2d9f75478108bd10fbe2e68ec1a5afa
Tobias Merkl <tobias@proudcommerce.com>
2024-01-08 20:51:53 +0100

    Fix content updates, Improve content without header

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ---
 
+## 1.1.1 - 2024-01-07
+- Fix content updates
+- Improve content without header
+
 ## 1.1.0 - 2024-01-07
 - Improve style comparison
 
--- MMM-MySQLData.js
+++ MMM-MySQLData.js
@@ -1,134 +1,136 @@
 Module.register("MMM-MySQLData", {
-    defaults: {
-        id: 1,
-        host: "",
-        port: 3306,
-        username: "",
-        password: "",
-        database: "",
-        values: [
-            {
-                "id": "",
-                "title": "",
-                "suffix": "",
-                "prefix": "",
-                "query": "",
-                "styles": {}
-            }
-        ],
-        data: {
-            "id": "",
-            "title": "",
-            "value": "",
-            "rawValue": "",
-            "styles": {}
-        }
-    },
+	defaults: {
+		id: 1,
+		host: "",
+		port: 3306,
+		username: "",
+		password: "",
+		database: "",
+		values: [
+			{
+				"id": "",
+				"title": "",
+				"suffix": "",
+				"prefix": "",
+				"query": "",
+				"styles": {}
+			}
+		],
+		data: {
+			"id": "",
+			"title": "",
+			"value": "",
+			"rawValue": "",
+			"styles": {}
+		}
+	},
 
-    getStyles() {
-        return [`${this.name}.css`];
-    },
+	getStyles() {
+		return [`${this.name}.css`];
+	},
 
-    start: function () {
-        this.sendSocketNotification("SET_CONFIG", {
-            config: this.config
-        });
-        this.getDbData();
-    },
+	start: function () {
+		this.sendSocketNotification("SET_CONFIG", {
+			config: this.config
+		});
+		this.getDbData();
+	},
 
-    getDbData() {
-        for (let data of this.config.values) {
-            this.sendSocketNotification("GET_DATA", {
-                id: this.config.id,
-                config: data
-            });
-            setInterval(() => {
-                this.sendSocketNotification("GET_DATA", {
-                    id: this.config.id,
-                    config: data
-                });
-            }, data.interval);
-        }
-    },
+	getDbData() {
+		for (let data of this.config.values) {
+			this.sendSocketNotification("GET_DATA", {
+				id: this.config.id,
+				config: data
+			});
+			setInterval(() => {
+				this.sendSocketNotification("GET_DATA", {
+					id: this.config.id,
+					config: data
+				});
+			}, data.interval);
+		}
+	},
 
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === "DATA_RESULT" && payload.instance === this.config.id) {
-            this.processData(payload);
-        }
-    },
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === "DATA_RESULT" && payload.instance === this.config.id) {
+			this.processData(payload);
+		}
+	},
 
-    processData: function (data) {
-        console.log(data);
-        if (data) {
-            this.createOrUpdateWrapper(data);
-            this.updateDom();
-        }
-    },
+	processData: function (data) {
+		console.log(data);
+		if (data) {
+			this.createOrUpdateWrapper(data);
+			this.updateDom();
+		}
+	},
 
-    wrappers: {},
+	wrappers: {},
 
-    createOrUpdateWrapper: function (data) {
-        let wrapper;
-        var id = this.config.id + '_' + data.id;
-        if (this.wrappers[id]) {
-            wrapper = this.wrappers[id];
-            wrapper.querySelector('.title').innerHTML = data.title;
-            this.updateContent(wrapper.querySelector('.content'), data);
-        } else {
-            wrapper = document.createElement("div");
-            wrapper.id = id;
-            wrapper.className = "wrapper";
+	createOrUpdateWrapper: function (data) {
+		let wrapper;
+		var id = this.config.id + '_' + data.id;
+		if (this.wrappers[id]) {
+			wrapper = this.wrappers[id];
+			if (data.title) {
+				wrapper.querySelector('.module-header').innerHTML = data.title;
+			}
+			this.updateContent(wrapper.querySelector('.module-content'), data);
+		} else {
+			wrapper = document.createElement("div");
+			wrapper.id = id;
+			wrapper.className = "wrapper";
 
-            if (data.title) {
-                let header = document.createElement("header");
-                header.className = "module-header";
-                header.innerHTML = data.title;
-                wrapper.appendChild(header);
-            }
+			if (data.title) {
+				let header = document.createElement("header");
+				header.className = "module-header";
+				header.innerHTML = data.title;
+				wrapper.appendChild(header);
+			}
 
-            let content = document.createElement("div");
-            content.className = "module-content";
-            this.updateContent(content, data);
-            wrapper.appendChild(content);
+			let content = document.createElement("div");
+			content.className = "module-content";
+			this.updateContent(content, data);
+			wrapper.appendChild(content);
 
-            this.wrappers[id] = wrapper;
-        }
-    },
+			this.wrappers[id] = wrapper;
+		}
+	},
 
-    updateContent: function (contentElement, data) {
-        contentElement.innerHTML = data.value;
-        contentElement.className = "module-content";
-        for (let style in data.styles) {
-            var $segments = data.styles[style].split(/\s+/);
-            var operator = $segments[0];
-            var leftOperand = parseFloat(data.rawValue);
-            var rightOperand = parseFloat($segments[1]);
-            var operators = {
-                '==': (a, b) => a == b,
-                '===': (a, b) => a === b,
-                '>': (a, b) => a > b,
-                '<': (a, b) => a < b,
-                '>=': (a, b) => a >= b,
-                '<=': (a, b) => a <= b,
-                '!=': (a, b) => a != b,
-                '!==': (a, b) => a !== b
-            };
-            if (operators[operator] && operators[operator](leftOperand, rightOperand)) {
-                contentElement.classList.add(style);
-            }
-        }
-    },
+	updateContent: function (contentElement, data) {
+		contentElement.innerHTML = data.value;
+		contentElement.className = "module-content";
+		for (let style in data.styles) {
+			var $segments = data.styles[style].split(/\s+/);
+			var operator = $segments[0];
+			var leftOperand = parseFloat(data.rawValue);
+			var rightOperand = parseFloat($segments[1]);
+			var operators = {
+				'==': (a, b) => a == b,
+				'===': (a, b) => a === b,
+				'>': (a, b) => a > b,
+				'<': (a, b) => a < b,
+				'>=': (a, b) => a >= b,
+				'<=': (a, b) => a <= b,
+				'!=': (a, b) => a != b,
+				'!==': (a, b) => a !== b
+			};
+			if (operators[operator] && operators[operator](leftOperand, rightOperand)) {
+				contentElement.classList.add(style);
+			}
+		}
+	},
 
-    getDom: function () {
-        var container = document.createElement("div");
-        for (let key in this.wrappers) {
-            container.appendChild(this.wrappers[key]);
-        }
+	getDom: function () {
+		var container = document.createElement("div");
+		for (let key in this.wrappers) {
+			container.appendChild(this.wrappers[key]);
+		}
 
-        return container;
-    },
+		return container;
+	},
 
-    getHeader: function () {
-        return this.data.header;
-    },
+	getHeader: function () {
+		return this.data.header;
+	},
 });
